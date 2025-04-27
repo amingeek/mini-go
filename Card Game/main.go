83e1a84dcc5c49b07f44fa4128f938b039fb802f
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-04-27 22:44:06 +0330

    Update main.go

--- Card Game/main.go
+++ Card Game/main.go
@@ -1,17 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	cards := deck{"A man from moon", newCard()}
-	fmt.Println(cards)
 	cards = append(cards, "After a bad day")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
+	cards.print()
 }
 
 func newCard() string {
