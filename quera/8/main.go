c70bda152c159b013ac111578b46fa986e437447
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-07-04 22:46:13 +0330

    end question 8

--- quera/8/main.go
+++ quera/8/main.go
@@ -1,34 +1,62 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 type FilterFunc func(int) bool
 type MapperFunc func(int) int
 
 func IsSquare(x int) bool {
-	//TODO
-	return false
+	if x < 0 {
+		return false
+	}
+	n := int(math.Sqrt(float64(x)))
+	return n*n == x
 }
 
 func IsPalindrome(x int) bool {
-	//TODO
-	return false
+	s := fmt.Sprintf("%d", x)
+	if len(s) > 0 && s[0] == '-' {
+		s = s[1:]
+	}
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
 }
 
 func Abs(num int) int {
 	//TODO
-	return 0
+	if num < 0 {
+		return -num
+	}
+	return num
 }
 
 func Cube(num int) int {
 	//TODO
-	return 0
+	return num * num * num
 }
 
 func Filter(input []int, f FilterFunc) []int {
-	//TODO
-	return nil
+	result := make([]int, 0, len(input))
+	for _, v := range input {
+		if f(v) {
+			result = append(result, v)
+		}
+	}
+	return result
 }
 
 func Map(input []int, m MapperFunc) []int {
 	//TODO
-	return nil
+	var result []int
+	for i := 0; i < len(input); i++ {
+		result = append(result, m(input[i]))
+	}
+	return result
 }
