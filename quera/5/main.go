62099bb37da503fa153a29937396f009ce4c0383
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-07-04 13:39:44 +0330

    end question 5

--- quera/5/main.go
+++ quera/5/main.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
+	var numbers []string
+	var check string
+	var nn int
+	count := make(map[string]string)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 
@@ -16,5 +21,31 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(n)
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		countrys := strings.Fields(scanner.Text())
+		count[countrys[1]] = countrys[0]
+	}
+
+	scanner.Scan()
+	nn, err = strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < nn; i++ {
+		scanner.Scan()
+		numbers = append(numbers, scanner.Text())
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		check = numbers[i][:3]
+		value, exists := count[check]
+		if exists {
+			fmt.Println(value)
+		} else {
+			fmt.Println("Invalid Number")
+		}
+	}
 }
