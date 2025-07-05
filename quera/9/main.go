f12f37d8c3bb88522f757f30e38e334350dea5ad
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-07-05 14:13:56 +0330

    end question 9

--- quera/9/main.go
+++ quera/9/main.go
@@ -2,17 +2,44 @@ package main
 
 func AddElement(numbers *[]int, element int) {
 	//TODO
+	*numbers = append(*numbers, element)
 }
 
 func FindMin(numbers *[]int) int {
 	//TODO
-	return 0
+	if len(*numbers) == 0 {
+		return 0
+	}
+
+	min := (*numbers)[0]
+	for _, num := range *numbers {
+		if num < min {
+			min = num
+		}
+	}
+
+	return min
 }
 
 func ReverseSlice(numbers *[]int) {
-	//TODO
+	if numbers == nil {
+		return
+	}
+	for i := 0; i < len(*numbers)/2; i++ {
+		j := len(*numbers) - 1 - i
+		(*numbers)[i], (*numbers)[j] = (*numbers)[j], (*numbers)[i]
+	}
 }
 
 func SwapElements(numbers *[]int, i, j int) {
-	//TODO
+	if numbers == nil {
+		return
+	}
+	if i < 0 || j < 0 || i >= len(*numbers) || j >= len(*numbers) {
+		return
+	}
+	if i == j {
+		return
+	}
+	(*numbers)[i], (*numbers)[j] = (*numbers)[j], (*numbers)[i]
 }
