481fd3d5f87d81d47958aa0e3a1179c865c9ef65
Amin <seyyed.amin.8809@gmail.com>
2025-08-29 20:29:49 +0330

    online book store init

--- quera/26/main.go
+++ quera/26/main.go
@@ -5,7 +5,20 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 )
+
+var requestData struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Borrow bool   `json:"borrow"`
+}
+
+type Book struct {
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Borrowed bool   `json:"borrowed"`
+}
 
 type Server struct {
 	port int
@@ -19,13 +32,42 @@ func NewServer(port string) *Server {
 	return &Server{port: intPort}
 }
 
-func book(w http.ResponseWriter, r *http.Request) {}
+func book(w http.ResponseWriter, r *http.Request) {
+	Books := []Book{}
+
+	switch r.Method {
+	case "POST":
+		title := strings.ToLower(r.URL.Query().Get("title"))
+		author := strings.ToLower(r.URL.Query().Get("author"))
+		if title == "" || author == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		for _, i := range Books {
+			if i.Title == title && i.Author == title {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+
+		Books = append(Books, Book{
+			Title:    title,
+			Author:   author,
+			Borrowed: false,
+		})
+
+	case "PUT":
+
+	}
+
+}
 
 func (s *Server) Start() {
+
 	http.HandleFunc("/book", book)
-	addr := fmt.Sprintf(":%d", s.port)
-	log.Printf("Starting server on %s\n", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	port := fmt.Sprintf(":%d", s.port)
+	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalf("server error: %v", err)
 	}
 }
