4733c5dd3597b56471dbff7c96ce11ac218b61e6
Amin <seyyed.amin.8809@gmail.com>
2025-08-31 00:12:27 +0330

    remove python sms bomber

--- quera/27/client/main.go
+++ quera/27/client/main.go
@@ -1,1 +1,37 @@
-package client
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{}
+	url := "http://localhost:8000/json"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Request failed with status code: %d\n", resp.StatusCode)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+	fmt.Printf("Response Body:\n%s\n", string(body))
+}
