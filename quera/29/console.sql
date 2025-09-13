688ee717ad8e261346bc3fae6806f45368d9bfa6
Amin <seyyed.amin.8809@gmail.com>
2025-09-14 00:25:40 +0330

    add sql code 29

---
+++ quera/29/console.sql
@@ -0,0 +1,34 @@
+use mydb;
+
+
+create table users (
+    id bigint auto_increment primary key ,
+    level enum('user', 'admin') not null default 'user',
+    name varchar(45) not null,
+    family varchar(45) not null ,
+    birth_date DATETIME
+
+)
+
+INSERT INTO users (level, name, family, birth_date)
+VALUES
+       (DEFAULT, 'amin', 'pourseyyedy', '2009-08-07 00:00:00');
+
+
+-- Section1
+CREATE TABLE events (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    date DATETIME,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Section2
+CREATE TABLE event_user (
+    user_id BIGINT UNSIGNED,
+    event_id BIGINT UNSIGNED,
+    PRIMARY KEY (user_id, event_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events(id)
+);
