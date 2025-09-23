d399ae2c87105f0478ec376b2982be6916f31314
Amin <seyyed.amin.8809@gmail.com>
2025-09-23 20:27:25 +0330

    end test echo singup 34

--- quera/34/main.go
+++ quera/34/main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
+
+type User struct {
+	Name        string `json:"name"`
+	Family      string `json:"family"`
+	Create_date time.Time
+}
 
 func WelcomeHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Welcome!")
 }
 
+func SignupHandler(c echo.Context) error {
+	user := new(User)
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+	user.Create_date = time.Now()
+	db := initializeDB()
+	db.Create(user)
+
+	return c.JSON(http.StatusCreated, user)
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", WelcomeHandler)
+	e.POST("/signup", SignupHandler)
 	e.Start(":8080")
 }
---
+++ quera/34/query.sql
@@ -0,0 +1,22 @@
+show databases;
+
+use mydb;
+
+select * from users;
+
+use quera;
+
+select * from users;
+
+
+use mydb;
+
+select *
+from users;
+
+
+delete
+from users
+where id in (5);
+
+select * from users;
