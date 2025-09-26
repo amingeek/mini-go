ac091fdadb1e0a1abbf4c8557a8dcf1ea4d09125
Amin <seyyed.amin.8809@gmail.com>
2025-09-26 17:47:27 +0330

    write again crud api echo 1

---
+++ quera/37/db.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func initDB() *gorm.DB {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_PORT := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
---
+++ quera/37/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type Product struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+func main() {
+	e := echo.New()
+	e.POST("/products", createProduct)
+	e.GET("/products", getProducts)
+	e.GET("/products/:id", getProduct)
+	e.PUT("/products/:id", updateProduct)
+	e.DELETE("/products/:id", deleteProduct)
+	e.Start(":8080")
+}
