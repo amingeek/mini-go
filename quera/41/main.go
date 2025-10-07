a4677d1fd56ba361ecb0e564acb57fcc3c5bb7f5
Amin <seyyed.amin.8809@gmail.com>
2025-10-07 22:39:12 +0330

    add client ws

--- quera/41/main.go
+++ quera/41/main.go
@@ -1,1 +1,32 @@
-package _1
+package main
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	url := "ws://localhost:8080/ws/hello"
+	log.Printf("connecting to %s", url)
+
+	connection, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatal("dial error:", err)
+	}
+	defer connection.Close()
+
+	sendMessage := "Quera college"
+	err = connection.WriteMessage(websocket.TextMessage, []byte(sendMessage))
+	if err != nil {
+		log.Println("write error:", err)
+	} else {
+		log.Println("send message:", sendMessage)
+	}
+	_, message, err := connection.ReadMessage()
+	if err != nil {
+		log.Println("read error:", err)
+		return
+	}
+	log.Printf("received message: %s", message)
+}
