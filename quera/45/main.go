f42962fc15f8c5a1a159db02dcdb27e7ef0e6b24
Amin <seyyed.amin.8809@gmail.com>
2025-10-16 17:46:42 +0330

    add q 46 gin

--- quera/45/main.go
+++ quera/45/main.go
@@ -11,7 +11,12 @@ func hello_get(c *gin.Context) {
 	c.String(http.StatusOK, "Hello, %s!", name)
 }
 
+func birthday(c *gin.Context) {
+	name := c.Query("birthday")
+	c.String(http.StatusOK, "Ha", name)
+}
 func main() {
 	e := gin.Default()
 	e.GET("/hello/:name", hello_get)
+
 }
